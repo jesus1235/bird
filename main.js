fe74711e500602a4dcc756fffed2da8d5a9e026a
FVI Guest <fviguest@fvi.edu>
2016-07-15 10:10:08 -0700

    did stuff

--- main.js
+++ main.js
@@ -28,6 +28,10 @@ var mainState = {
 
       this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 
+      this.score = 0;
+      this.labelScore = game.add.text(20, 20, "0",
+          { font: "30px Arial", fill: "#ffffff" });
+
 
     },
 
@@ -54,6 +58,8 @@ var mainState = {
 
       pipe.checkWorldBounds = true;
       pipe.outOfBoundsKill = true;
+
+
 
 
     },
